5d98b09ee4ddc9ed62fa2f6707ca358ddc46bc6e
MANISH-GIRI <manish.giri.me@gmail.com>
2019-12-11 20:15:16 -0500

    Other changes

--- c-cpp/array-adt/merge_arrays.c
+++ c-cpp/array-adt/merge_arrays.c
@@ -46,9 +46,6 @@ int main(int argc, char const *argv[])
     struct Array arr1 = {{2, 9, 21, 28, 35}, 10, 5};
     struct Array arr2 = {{2, 3, 16, 18, 28}, 10, 5};
     struct Array *arr3 = Merge(&arr1, &arr2);
-
-    Merge(&arr1, &arr2);
     Display(*arr3);
-
     return 0;
 }
